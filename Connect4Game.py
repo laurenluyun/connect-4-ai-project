751ef823c402a818d08c3c0a8c0cf8ce84b13b3a
whuang@sprucetech.com <floridian567@gmail.com>
2025-04-09 12:07:16 -0400

    made a functional main menu

--- Connect4Game.py
+++ Connect4Game.py
@@ -28,6 +28,7 @@ class Connect4Game:
 
         self.game_over = False
         self.turn = random.choice([0, 1])
+        self.turn = 0
         self.board = self.create_board()
 
         # initialize graphical interface
@@ -52,36 +53,18 @@ class Connect4Game:
     # generate a main menu on the screen
     def menu(self):
         """Ask the user to select the game mode."""
-        # while True:
-        #     mode = input("Choose game mode: (1) Player vs. Player, (2) Player vs. AI: ")
-        #     if mode in {"1", "2"}:
-        #         if mode == "1":
-        #             self.game_mode = "PvP"
-        #             break
-        #         else:
-        #             self.game_mode = "PvAI"
-        #             difficulty = input("Choose game difficulty: (1) Easy, (2) Hard: ")
-        #             if difficulty in {"1", "2"}:
-        #                 if difficulty == "1":
-        #                     self.difficulty = "easy"
-        #                     break
-        #                 else:
-        #                     self.difficulty = "hard"
-        #                     break
-        #             else:
-        #                 print("Invalid choice. Enter 1 or 2.")
-        #     else:
-        #         print("Invalid choice. Enter 1 or 2.")
-
         while True:
+            self.reset_game()
+            self.game_mode = None
+            self.difficulty = None
             self.screen.fill(self.gray)
             mouse_position = pygame.mouse.get_pos()
             connect_4_text = self.screen_font.render(f"Connect 4", 1, self.blue)
-            self.screen.blit(connect_4_text, (self.screen_width / 4 - 20, 10))
+            self.screen.blit(connect_4_text, (self.screen_width / 4 - 20, 60))
             main_menu_text = self.screen_font.render(f"Main Menu", 1, self.blue)
-            self.screen.blit(main_menu_text, (self.screen_width / 4 - 20, 100))
-            play_button = Button(position=(self.screen_width / 2, 300), text_input="Play", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
-            quit_button = Button(position=(self.screen_width / 2, 500), text_input="Quit", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            self.screen.blit(main_menu_text, (self.screen_width / 4 - 20, 150))
+            play_button = Button(position=(self.screen_width / 2, 350), text_input="Play", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            quit_button = Button(position=(self.screen_width / 2, 550), text_input="Quit", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
 
             for button in [play_button, quit_button]:
                 button.change_button_color(mouse_position)
@@ -93,8 +76,7 @@ class Connect4Game:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if play_button.check_for_button_input(mouse_position):
-                        print()
-                        # self.start_game()
+                        self.one_player_or_two_player_menu()
                     if quit_button.check_for_button_input(mouse_position):
                         pygame.quit()
                         sys.exit()
@@ -104,11 +86,76 @@ class Connect4Game:
 
     # creating play screen to choose between 1 player and 2 player options
     def one_player_or_two_player_menu(self):
-        print()
+        while True:
+            self.screen.fill(self.gray)
+            mouse_position = pygame.mouse.get_pos()
+            single_player_button = Button(position=(self.screen_width / 2, 150), text_input="1 Player", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            two_players_button = Button(position=(self.screen_width / 2, 350), text_input="2 Players", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            back_button = Button(position=(self.screen_width / 2, 550), text_input="Back", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+
+            for button in [single_player_button, two_players_button, back_button]:
+                button.change_button_color(mouse_position)
+                button.update_button(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if single_player_button.check_for_button_input(mouse_position):
+                        self.game_mode = "PvAI"
+                        self.choose_difficulty_level_menu()
+                        if self.game_over:
+                            return
+                    if two_players_button.check_for_button_input(mouse_position):
+                        self.game_mode = "PvP"
+                        self.start_game()
+                        return
+                    if back_button.check_for_button_input(mouse_position):
+                        return
+
+            pygame.display.update()
 
     # creating 1 player screen to choose between easy, medium, hard mode AI
     def choose_difficulty_level_menu(self):
-        print()
+        while True:
+            self.screen.fill(self.gray)
+            mouse_position = pygame.mouse.get_pos()
+            difficulty_screen_font = pygame.font.SysFont("monospace", 60)
+            difficulty_mode_text = difficulty_screen_font.render(f"AI Difficulty Mode", 1, self.blue)
+            self.screen.blit(difficulty_mode_text, (30, 60))
+            easy_mode_button = Button(position=(self.screen_width / 2, 200), text_input="Easy",
+                                          font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            medium_mode_button = Button(position=(self.screen_width / 2, 350), text_input="Medium",
+                                        font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+            hard_mode_button = Button(position=(self.screen_width / 2, 500), text_input="Hard", font=self.screen_font,
+                                 base_color=self.blue, hovering_color=self.green)
+            back_button = Button(position=(self.screen_width / 2, 650), text_input="Back", font=self.screen_font, base_color=self.blue, hovering_color=self.green)
+
+
+            for button in [easy_mode_button, medium_mode_button,hard_mode_button, back_button]:
+                button.change_button_color(mouse_position)
+                button.update_button(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if easy_mode_button.check_for_button_input(mouse_position):
+                        self.difficulty = "easy"
+                        self.start_game()
+                        return
+                    if medium_mode_button.check_for_button_input(mouse_position):
+                        print()
+                    if hard_mode_button.check_for_button_input(mouse_position):
+                        self.difficulty = "hard"
+                        self.start_game()
+                        return
+                    if back_button.check_for_button_input(mouse_position):
+                        return
+
+            pygame.display.update()
 
     def draw_grid(self):
         for column in range(self.num_columns):
@@ -152,7 +199,7 @@ class Connect4Game:
             return True
         return False
 
-    def ai_move(self, difficulty):
+    def ai_move(self):
         if self.difficulty == 'easy':
             # AI selects the best move
             col, _ = self.ai.minimax(self.board, self.DEPTH, self.ALPHA, self.BETA, True)
@@ -161,13 +208,19 @@ class Connect4Game:
         # AI makes the move
         self.make_move(col, self.AI_PIECE)
 
+    def reset_game(self):
+        self.game_over = False
+        self.board = self.create_board()
+
     def start_game(self):
+        self.reset_game()
+        self.screen.fill(self.white)
         self.draw_board()
         while not self.game_over:
             # If it's AI's turn, make the move automatically
             if self.game_mode == "PvAI" and self.turn == 1 and not self.game_over:
                 pygame.time.wait(500)  # Add a slight delay for realism
-                self.ai_move(self.difficulty)
+                self.ai_move()
                 self.turn = 0  # Switch back to player
                 self.draw_board()
                 if self.game_over:
@@ -197,10 +250,11 @@ class Connect4Game:
                         self.draw_board()
                         if self.game_over:
                             pygame.time.wait(4000)
+                            return
+
 
 
 # Start the game
 if __name__ == "__main__":
     game = Connect4Game()
     game.menu()
-    game.start_game()
